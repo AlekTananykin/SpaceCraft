81a067eb7709b907df67d44b7fddaae43ca92e82
AlekTananykin <70412225+AlekTananykin@users.noreply.github.com>
2022-06-05 23:16:26 +0300

    project init

---
+++ Assets/Scripts/Main/SolarSystemNetworkManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Characters;
+using UnityEngine;
+using Mirror;
+
+
+namespace Main
+{
+    public class SolarSystemNetworkManager : NetworkManager
+    {
+        //public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        //{
+        //    base.OnServerAddPlayer(conn);
+
+        //    var spawnTransform = GetStartPosition();
+            
+        //    var player = Instantiate(playerPrefab, 
+        //        spawnTransform.position, spawnTransform.rotation);
+
+        //    //_players.Add(conn.connectionId, player.GetComponent<ShipController>());
+        //    //_players[conn.connectionId].onPlayerCollided += OnPlayerCollided;
+
+        //    NetworkServer.AddPlayerForConnection(conn, player);
+
+        //}
+
+        
+    }
+}
