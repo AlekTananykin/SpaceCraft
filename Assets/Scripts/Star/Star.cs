af1d8a7480787deb0757e71b584a0d1568ec8347
AlekTananykin <70412225+AlekTananykin@users.noreply.github.com>
2022-06-22 23:58:33 +0300

    start

--- Assets/Scripts/Star/Star.cs
+++ Assets/Scripts/Star/Star.cs
@@ -6,10 +6,13 @@ public class Star : MonoBehaviour
 {
     private Mesh _mesh;
 
-    [SerializeField] private Vector3[] _points;
+    [SerializeField] private ColorPoint _center;
+    [SerializeField] private ColorPoint[] _points;
     [SerializeField] private int _frequency = 1;
 
     private Vector3[] _vertices;
+    private Color[] _colors;
+
     private int[] _triangles;
 
 
@@ -25,20 +28,26 @@ public class Star : MonoBehaviour
         {
             _frequency = 1;
         }
-        _points ??= Array.Empty<Vector3>();
+        _points ??= Array.Empty<ColorPoint>();
+
         var numberOfPoints = _frequency * _points.Length;
         _vertices = new Vector3[numberOfPoints + 1];
+        _colors = new Color[numberOfPoints + 1];
+
         _triangles = new int[numberOfPoints * 3];
 
         if (numberOfPoints >= 3)
         {
+            _vertices[0] = _center.Position;
+            _colors[0] = _center.Color;
             var angle = -360f / numberOfPoints;
             for (int repetitions = 0, v = 1, t = 1; repetitions < _frequency;
                 repetitions++)
             {
                 for (var p = 0; p < _points.Length; p++, v++, t += 3)
                 {
-                    _vertices[v] = Quaternion.Euler(0f, 0f, angle * (v - 1)) * _points[p];
+                    _vertices[v] = Quaternion.Euler(0f, 0f, angle * (v - 1)) * _points[p].Position;
+                    _colors[v] = _points[p].Color;
                     _triangles[t] = v;
                     _triangles[t + 1] = v + 1;
                 }
